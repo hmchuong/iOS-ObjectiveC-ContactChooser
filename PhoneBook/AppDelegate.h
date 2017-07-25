0ef0c90928cf20a25b0be3e8accf89227df6a112
chuonghm <chuonghm@Admins-MacBook-Pro.local>
2017-07-25 18:31:45 +0700

    init

---
+++ PhoneBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneBook
+//
+//  Created by chuonghm on 7/25/17.
+//  Copyright © 2017 VNG Corp., Zalo Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
