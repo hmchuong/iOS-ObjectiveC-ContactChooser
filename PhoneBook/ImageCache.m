2b9e273b6b925594e5c1d2046142a8b45ecf9f65
Huỳnh Minh Chương <chuonghuynh@vms-Mac.local>
2017-08-03 06:52:54 +0700

    update image

--- PhoneBook/ImageCache.m
+++ PhoneBook/ImageCache.m
@@ -8,10 +8,16 @@
 
 #import "ImageCache.h"
 
+FOUNDATION_STATIC_INLINE NSUInteger UIImageCost(UIImage *image) {
+    return image.size.height * image.size.width * image.scale * image.scale;
+}
+
 @interface ImageCache()
 
 @property (strong, nonatomic) NSFileManager *fileManager;
 @property (strong, nonatomic) NSCache *memCache;
+@property (strong, nonatomic) dispatch_queue_t ioQueue;
+@property (strong, nonatomic) NSString *dirPath;
 
 @end
 
@@ -19,8 +25,31 @@
 
 - (instancetype)init {
     self = [super init];
-    _fileManager = [NSFileManager defaultManager];
     _memCache = [[NSCache alloc]init];
+    [_memCache setTotalCostLimit:500];
+    _ioQueue = dispatch_queue_create("com.vn.zalo.ImageCache", DISPATCH_QUEUE_SERIAL);
+    
+    // I/O
+    dispatch_async(_ioQueue, ^{
+        // File manager
+        _fileManager = [NSFileManager defaultManager];
+        
+        // Create directory path
+        NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *docsPath = [dirPaths objectAtIndex:0];
+        _dirPath = [docsPath stringByAppendingString:@"/com.vn.zalo.ImageCache"];
+        
+        // Create directory if not exist
+        if (![_fileManager fileExistsAtPath:_dirPath]) {
+            
+            [_fileManager createDirectoryAtPath:_dirPath
+                    withIntermediateDirectories:NO
+                                     attributes:nil
+                                          error:nil];
+        }
+    });
+    
+    // clear memory cache if mem warning
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(clearMemory)
                                                  name:UIApplicationDidReceiveMemoryWarningNotification
@@ -39,13 +68,10 @@
 
 - (void)storeImage:(UIImage *)image withKey:(NSString *)key {
     NSString *filePath = [self getFilePathFromKey:key];
-#ifdef DEBUG
-    if ([_fileManager fileExistsAtPath:filePath]) {
-        NSLog(@"Key %@ has already existed",key);
-    }
-#endif
     NSData *imageData = UIImagePNGRepresentation(image);
-    [imageData writeToFile:filePath atomically:YES];
+    dispatch_async(_ioQueue, ^{
+        [imageData writeToFile:filePath atomically:YES];
+    });
 }
 
 - (UIImage *)imageFromKey:(NSString *)key {
@@ -58,10 +84,15 @@
 
 - (UIImage *)loadFromDiskCacheWithKey:(NSString *)key {
     NSString *filePath = [self getFilePathFromKey:key];
-    NSData *imageData = [NSData dataWithContentsOfFile:filePath];
+    
+    NSData __block *imageData;
+    dispatch_sync(_ioQueue, ^{
+        imageData = [NSData dataWithContentsOfFile:filePath];
+    });
+    
     UIImage *image = [UIImage imageWithData:imageData];
     if (image) {
-        [_memCache setObject:image forKey:key];
+        [_memCache setObject:imageData forKey:key cost:UIImageCost(image)];
     }
     return image;
 }
@@ -71,12 +102,10 @@
 }
 
 - (NSString *)getFilePathFromKey:(NSString *)key {
-    NSString *docsDir;
-    NSArray *dirPaths;
-    
-    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    docsDir = [dirPaths objectAtIndex:0];
-    NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:key]];
+    NSString __block *databasePath;
+    dispatch_sync(_ioQueue, ^{
+        databasePath = [[NSString alloc] initWithString: [_dirPath stringByAppendingPathComponent:key]];
+    });
     return databasePath;
 }
 
